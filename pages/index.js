bee9e2de1aef5ced4255b676816f6c6600dab642
ss251 <ss251@uw.edu>
2022-12-03 07:44:12 +0530

    landing page message

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,15 @@
 export default function Home() {
   return (
-    <div className="text-4xl text-white font-bold bg-black h-screen"></div>
+    <div className="bg-black flex justify-center">
+      <div
+        className="flex mt-24 text-6xl font-semibold 
+            bg-gradient-to-r bg-clip-text text-transparent 
+            from-indigo-500 via-purple-500 to-indigo-500
+            animate-text ml-48 mr-48
+            "
+      >
+        Make seamless payments with BOB stablecoin and the zkBob dApp!
+      </div>
+    </div>
   );
 }
