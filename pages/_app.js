d13b86772c0af64766c0cbc57cdb6bd3fc9b7725
ss251 <ss251@uw.edu>
2022-12-04 03:40:23 +0530

    display transactions and withhdraw funds to zk account

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,36 @@
 import "../styles/globals.css";
 import Navbar from "../components/Navbar";
 import { ThirdwebProvider } from "@thirdweb-dev/react";
+import { useState, useEffect } from "react";
 
 import { ChainId } from "@thirdweb-dev/sdk";
 
 const activeChainId = ChainId.Polygon;
 
 function App({ Component, pageProps }) {
+  const [apiKey, setApiKey] = useState("");
+
+  const API_URL = "http://localhost:9081";
+
+  useEffect(() => {
+    if (
+      localStorage.getItem("api_key") === null ||
+      localStorage.getItem("api_key") === undefined
+    ) {
+      fetch(API_URL + "/getKey", {
+        method: "GET",
+        mode: "cors",
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+        },
+      })
+        .then((res) => res.json())
+        .then((json) => {
+          localStorage.setItem("api_key", json.Key._id);
+        });
+    }
+  }, [apiKey]);
+
   return (
     <ThirdwebProvider desiredChainId={activeChainId}>
       <div className="bg-black h-screen">
--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,16 +1,39 @@
-import React from "react";
-import data from "../api.json";
+import React, { useEffect, useState } from "react";
 import { useAddress } from "@thirdweb-dev/react";
 
 export default function Dashboard() {
   const address = useAddress();
 
-  const DisplayData = data.record.getDashboardData.txs.map((info) => {
+  const [data, setData] = useState({ transactions: [] });
+  const [input, setInput] = useState("");
+
+  const API_URL = "http://localhost:9081";
+
+  const GetData = () => {
+    fetch(API_URL + `/getDashboardData/${localStorage.getItem("api_key")}`)
+      .then((res) => res.json())
+      .then((json) => {
+        setData(json);
+      });
+    //
+  };
+
+  const withDrawFunds = () => {
+    fetch(
+      API_URL + `/withdrawAmountTo/${input}/${localStorage.getItem("api_key")}`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        alert(json.message);
+      });
+  };
+
+  const DisplayData = data.transactions.map((transaction) => {
     return (
-      <tr key={info.zkAddress} className="text-white">
-        <td>{info.zkAddress}</td>
-        <td>{info.Amount}</td>
-        <td>{info.timestamp}</td>
+      <tr key={transaction.zkAddress} className="text-white">
+        <td>{transaction.zkAddress}</td>
+        <td>{Number(transaction.amount) / 1000000000} BOB</td>
+        <td>{transaction.timestamp}</td>
       </tr>
     );
   });
@@ -34,8 +57,9 @@ export default function Dashboard() {
       >
         Transactions
       </div>
+      <button onClick={GetData}>Refresh</button>
       <div className="flex flex-col flex-grow items-center">
-        <div className="flex justify-center h-40 overflow-y-auto">
+        <div className="flex justify-center h-40 overflow-y-auto mb-8">
           <table className="table table-auto border border-white border-separate border-spacing-x-8 border-spacing-y-4 rounded-md">
             <thead>
               <tr className="text-white">
@@ -44,8 +68,32 @@ export default function Dashboard() {
                 <th>timestamp</th>
               </tr>
             </thead>
-            <tbody>{DisplayData}</tbody>
+            <tbody>{data !== null && data !== undefined && DisplayData}</tbody>
           </table>
+        </div>
+      </div>
+      <div
+        className="text-3xl font-semibold overflow-hidden
+            bg-gradient-to-r bg-clip-text  text-transparent 
+            from-gray-500 via-gray-400 to-gray-300 flex flex-col items-center"
+      >
+        Balance: {Number(data.balance) / 1000000000}
+        <button
+          className="hover:bg-gradient-to-r
+            from-indigo-500 via-purple-500 to-indigo-500
+            animate-text bg-gradient-to-r from gray-500 via gray-400 to-gray-300
+            text-white border-indigo-500 border-2 rounded-lg p-4 shadow-lg font-bold mt-4"
+          onClick={() => withDrawFunds(input)}
+        >
+          Withdraw
+        </button>
+        <div className="overflow-hidden resize-x mt-4">
+          <textarea
+            className="resize rounded-md text-black h-8"
+            onInput={(e) => setInput(e.target.value)}
+            value={input}
+            placeholder={"Enter ZK address"}
+          ></textarea>
         </div>
       </div>
     </div>
